fb3ed63d76ab4332bd93468afb34c067756918aa
Virgeel <rafisultan245@gmail.com>
2025-07-03 17:10:37 +0700

    first init

---
+++ resources/views/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title> SPINDO Learning Management System</title>
+    </head>
+
+    @include('main.partials.navbar')
+
+    <body>
+
+        @yield('body')
+        
+    </body>
+</html>
